1d0d940cdb9c053bade674fff2c28c7e79195f3a
Matheus Oliveira <oliveira.matheusde@gmail.com>
2019-01-08 02:45:50 +0000

    Ionic Course: Native device features

--- ionicStarWarsApp/src/app/app.module.ts
+++ ionicStarWarsApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,7 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    EmailComposer
   ],
   bootstrap: [AppComponent]
 })
--- ionicStarWarsApp/src/app/pages/films-detail/films-detail.page.ts
+++ ionicStarWarsApp/src/app/pages/films-detail/films-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @Component({
   selector: 'app-films-detail',
@@ -10,7 +11,7 @@ import { ApiService } from 'src/app/services/api.service';
 export class FilmsDetailPage implements OnInit {
   film: any;
 
-  constructor(private activatedRoute: ActivatedRoute, private apiService: ApiService) {
+  constructor(private activatedRoute: ActivatedRoute, private apiService: ApiService, private emailComposer: EmailComposer) {
   }
 
   ngOnInit() {
@@ -19,4 +20,15 @@ export class FilmsDetailPage implements OnInit {
     this.apiService.getFilm(id).subscribe((data: any) => this.film = data);
   }
 
+  shareFilm() {
+    const { title, opening_crawl } = this.film;
+
+    let email = {
+      to: 'oliveira.matheusde@gmail.com',
+      subject: `I love his one ${title}`,
+      body: `Can you remember the opening?<br><br>\\${opening_crawl}\\`
+    };
+
+    this.emailComposer.open(email);
+  }
 }
